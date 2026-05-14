e293f649181dd3c4feec9b178bc1eaad56ed4069
Mez0ry <612315123@mail.ru>
2026-05-14 17:45:17 +0300

    Implemented constructors for 'Vertex2D'

--- include/VertexBuffer.hpp
+++ include/VertexBuffer.hpp
@@ -1,11 +1,16 @@
 #ifndef __VERTEX_BUFFER_HPP__
 #define __VERTEX_BUFFER_HPP__
 #include <cstdint>
+#include <initializer_list>
+
+struct Vertex2D;
 
 class VertexBuffer{
 private:
     uint32_t m_BufferId;
 public:
+    VertexBuffer(const std::initializer_list<Vertex2D>& vertex);
+    VertexBuffer(const Vertex2D& vertex);
     VertexBuffer(const void* data, uint32_t size);
     ~VertexBuffer() = default;
 
--- src/VertexBuffer.cpp
+++ src/VertexBuffer.cpp
@@ -1,11 +1,27 @@
 #include "VertexBuffer.hpp"
 #include <glad/gl.h>
+#include "Vertex.hpp"
 
-VertexBuffer::VertexBuffer(const void *data, uint32_t size)
+VertexBuffer::VertexBuffer(const std::initializer_list<Vertex2D> &vertex)
 {
+    std::vector<Vertex2D> verticies(vertex);
+
     glGenBuffers(1, &m_BufferId);
     glBindBuffer(GL_ARRAY_BUFFER, m_BufferId);
-    glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, verticies.size() * sizeof(Vertex2D), verticies.data(), GL_STATIC_DRAW);
+
+}
+
+VertexBuffer::VertexBuffer(const Vertex2D &vertex) {
+    glGenBuffers(1, &m_BufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, m_BufferId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex2D), &vertex, GL_STATIC_DRAW);
+}
+
+VertexBuffer::VertexBuffer(const void *data, uint32_t size) {
+  glGenBuffers(1, &m_BufferId);
+  glBindBuffer(GL_ARRAY_BUFFER, m_BufferId);
+  glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 }
 
 void VertexBuffer::Bind() const {

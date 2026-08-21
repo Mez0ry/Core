398cd92342eebcd89db9dcb2335ece7206a25662
Mez0ry <612315123@mail.ru>
2026-08-21 11:24:19 +0300

    Small improvemenets

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -9,6 +9,8 @@ class Shader;
 class Color;
 
 class Window;
+class Mesh;
+class Sprite2D;
 
 class Renderer{
 private:
@@ -17,10 +19,12 @@ public:
     ~Renderer();
 
     void SetClearColor(const Color& color);
-    void Clear(uint32_t clear_flags = GL_COLOR_BUFFER_BIT) const;
+    void Clear(uint32_t clear_flags = GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) const;
 
     void Render(const VertexArray& va, const IndexBuffer& ib, const Shader& shader, GLenum render_mode = GL_TRIANGLES, uint32_t data_type = GL_UNSIGNED_INT);
 
+    void Render(const Mesh& mesh, const Shader& shader, GLenum render_mode = GL_TRIANGLES, uint32_t data_type = GL_UNSIGNED_INT);
+    void Render(const Sprite2D& sprite);
     void SwapBuffers(Window& window);
 private:
 };
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -7,6 +7,10 @@
 #include "IndexBuffer.hpp"
 #include "Shader.hpp"
 
+#include "Mesh.hpp"
+#include "Uniform.hpp"
+#include "Sprite2D.hpp"
+
 #include "Color.hpp"
 
 void Renderer::Clear(uint32_t clear_flags) const{
@@ -25,7 +29,63 @@ void Renderer::Render(const VertexArray &va, const IndexBuffer &ib, const Shader
     shader.UseShader();
     va.Bind();
     ib.Bind();
+
     glDrawElements(render_mode, ib.GetCount(), data_type, nullptr);
+    
+    va.UnBind();
+    ib.UnBind();
+}
+
+#include <iostream>
+
+void Renderer::Render(const Mesh &mesh, const Shader &shader, GLenum render_mode, uint32_t data_type)
+{
+    if(!shader.IsValid()){
+        //handle
+    }
+    shader.UseShader();
+    
+    auto& textures = mesh.GetTextures();
+    
+    unsigned int diffuse_nr = 1;
+    unsigned int specular_nr = 1;
+    for(unsigned int i = 0; i < textures.size(); i++)
+    {        
+        std::string uniform_name;
+        switch (textures[i].type)
+        {
+            case TextureType::Diffuse:
+                uniform_name = "u_diffuse" + std::to_string(diffuse_nr++);
+                break;
+            case TextureType::Specular:
+                uniform_name = "u_specular" + std::to_string(specular_nr++);
+                break;
+            case TextureType::Base:
+                uniform_name = textures[i].str_type; 
+                break;
+            default:
+                uniform_name = textures[i].str_type;
+        }
+
+        textures[i].texture->Bind(i);
+        Uniform::SetShaderUniformInt1(shader, uniform_name, i);
+    }
+
+    auto& va = mesh.GetVertexArray();
+    auto& ib = mesh.GetIndexBuffer();
+    va->Bind();
+    ib->Bind();
+    
+    glDrawElements(render_mode, ib->GetCount(), data_type, nullptr);
+    va->UnBind();
+
+    glActiveTexture(GL_TEXTURE0);
+}
+
+void Renderer::Render(const Sprite2D &sprite)
+{
+    sprite.Update();
+    this->Render(sprite.GetMesh(), *sprite.GetShader());
 }
 
 void Renderer::SwapBuffers(Window &window)

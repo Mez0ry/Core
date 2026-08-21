e3a86dcb373386af548b56b5d9de7f1cd1c119e8
Mez0ry <612315123@mail.ru>
2026-08-21 11:23:44 +0300

    Refactoring of 'Mesh' module

--- include/Mesh.hpp
+++ include/Mesh.hpp
@@ -1,21 +1,70 @@
 #ifndef __MESH_HPP__
 #define __MESH_HPP__
+#include "Base.hpp"
 
 #include "VertexArray.hpp"
 #include "IndexBuffer.hpp"
 #include "Shader.hpp"
+#include "Texture.hpp"
+
+#include <vector>
 
 class VertexArray;
 class IndexBuffer; 
 class Shader;
+class Texture;
+
+enum class TextureType {
+    Diffuse,
+    Specular,
+    Normal,
+    Roughness,
+    Metalness,
+    Base,
+};
+
+struct MeshTexture {
+    MeshTexture(const Ref<Texture> texture_, TextureType type_) : texture(texture_), type(type_), str_type(TypeToString(type)) {}
+    const Ref<Texture> texture;  
+    TextureType type;
+    std::string str_type;
+private:
+    std::string TypeToString(TextureType type) const {
+        switch (type) {
+            case TextureType::Diffuse: return "texture_diffuse";
+            case TextureType::Specular: return "texture_specular";
+            case TextureType::Normal: return "texture_normal";
+            case TextureType::Roughness: return "texture_roughness";
+            case TextureType::Metalness: return "texture_metalness";
+            case TextureType::Base: return "u_texture";
+            default: return "unknown";
+        }
+    }
+};
+
+class Vector2D;
 
 class Mesh{
-private:
-    VertexArray m_VertexArray;
-    IndexBuffer m_IndexBuffer;
-    Shader m_Shader;
+protected:
+    Ref<VertexArray> m_VertexArray;
+    Ref<IndexBuffer> m_IndexBuffer;
+    std::vector<MeshTexture> m_Textures;
 public:
-    explicit Mesh(const VertexArray &va, const IndexBuffer &ib, Shader &&shader);
+    Mesh() = default;
+    explicit Mesh(const Ref<VertexArray> va, const Ref<IndexBuffer> ib);
+    explicit Mesh(const Ref<VertexArray> va, const Ref<IndexBuffer> ib, const std::vector<MeshTexture>& textures);
+
+    virtual ~Mesh(){}
+
+    const Ref<VertexArray> GetVertexArray() const;
+    const Ref<IndexBuffer> GetIndexBuffer() const;
+    const std::vector<MeshTexture> GetTextures() const;
+};
+
+class QuadMesh final : public Mesh{
 private:
+    Mesh BuildQuadMesh(std::vector<MeshTexture> textures) const;
+public:
+    QuadMesh(std::vector<MeshTexture> textures);
 };
 #endif //!__MESH_HPP__
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,6 +1,52 @@
 #include "Mesh.hpp"
+#include "BufferLayout.hpp"
+#include "VertexBuffer.hpp"
 
-Mesh::Mesh(const VertexArray &va, const IndexBuffer &ib, Shader &&shader) : m_VertexArray(va), m_IndexBuffer(ib), m_Shader(std::move(shader))
+Mesh::Mesh(const Ref<VertexArray> va, const Ref<IndexBuffer> ib) : m_VertexArray(va), m_IndexBuffer(ib)
 {
 
+}
+
+Mesh::Mesh(const Ref<VertexArray> va, const Ref<IndexBuffer> ib, const std::vector<MeshTexture> &textures) : m_VertexArray(va), m_IndexBuffer(ib), m_Textures(textures)
+{
+}
+
+const Ref<VertexArray> Mesh::GetVertexArray() const
+{
+    return m_VertexArray;
+}
+
+const Ref<IndexBuffer> Mesh::GetIndexBuffer() const
+{
+    return m_IndexBuffer;
+}
+
+const std::vector<MeshTexture> Mesh::GetTextures() const
+{
+    return m_Textures;
+}
+
+Mesh QuadMesh::BuildQuadMesh(std::vector<MeshTexture> textures) const
+{
+    BufferLayout vb_layout;
+    vb_layout.Push<Vertex2D>(false);
+
+    std::vector<Vertex2D> vertices_pixel{
+        Vertex2D(glm::vec2(-0.5f, -0.5f), glm::vec3(0.f, 0.f, 0.f), glm::vec2(0.f, 0.f)),
+        Vertex2D(glm::vec2(0.5f, -0.5f), glm::vec3(0.f, 0.f, 0.f), glm::vec2(1.0f, 0.0f)),
+        Vertex2D(glm::vec2(0.5f, 0.5f), glm::vec3(0.f, 0.f, 0.f), glm::vec2(1.0f, 1.0f)),
+        Vertex2D(glm::vec2(-0.5f, 0.5f), glm::vec3(0.f, 0.f, 0.f), glm::vec2(0.0f, 1.0f))
+    };
+
+    unsigned int indicies[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    return Mesh(MakeReference<VertexArray>(MakeReference<VertexBuffer>(vertices_pixel), vb_layout), MakeReference<IndexBuffer>(indicies, 6), textures);
+}
+
+QuadMesh::QuadMesh(std::vector<MeshTexture> textures) : Mesh(BuildQuadMesh(textures))
+{
+    
 }
